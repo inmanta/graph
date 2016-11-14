756b2e070106c72ee804475257153e40ef37c9fb
Wouter De Borger <wouter.deborger@inmanta.com>
2016-11-14 14:40:00 +0100

    Fixed module for new compiler

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -19,250 +19,274 @@
 
 from inmanta.export import export
 from inmanta.ast.attribute import RelationAttribute
-from inmanta.execute.proxy import DynamicProxy 
+from inmanta.execute.proxy import DynamicProxy
 
-import os, subprocess
+import os
+import subprocess
+import re
 
-#FIXME: hack to prevent core bugs from triggering https://github.com/bartv/imp/issues/2
+# FIXME: hack to prevent core bugs from triggering https://github.com/bartv/imp/issues/2
+
+
 def getAttributeName(attrib):
     if isinstance(attrib, Enum):
         return "Enum"
     return attrib.get_name()
 
-#FIXME: hack to prevent core bugs from triggering https://github.com/bartv/imp/issues/2
+# FIXME: hack to prevent core bugs from triggering https://github.com/bartv/imp/issues/2
+
+
 def getAttribType(attrib):
     if isinstance(attrib.get_type(), Enum):
         return "Enum"
-     
-    return str(attrib.get_type()).replace('<','').replace('>','').replace(' ','_')
+
+    return str(attrib.get_type()).replace('<', '').replace('>', '').replace(' ', '_')
 
 
-#FIXME: do not put tuples of different length into one dict
+class Node(object):
+
+    def __init__(self, id, **props):
+        self.id = id
+        self.props = props
+
+    def to_dot(self):
+        options = ",".join(['%s="%s"' % x for x in self.props.items()])
+        return '"%s" [%s];\n' % (self.id, options)
+
+
+class Relation(object):
+
+    def __init__(self, id, fro, to, **props):
+        self.id = id
+        self.fro = fro
+        self.to = to
+        self.props = props
+
+    def to_dot(self):
+        if len(self.props) == 0:
+            return '"%s" -- "%s";\n' % (id(self.fro), id(self.to))
+        else:
+            options = ",".join(['%s="%s"' % x for x in self.props.items()])
+            return '"%s" -- "%s" [%s];\n' % (id(self.fro), id(self.to), options)
+
+
+# FIXME: do not put tuples of different length into one dict
 class graphCollector(object):
+
     def __init__(self):
         self.relations = dict()
-        self.parents=dict()
-        self.nodes = set()
-        
+        self.parents = dict()
+        self.nodes = {}
+
     def addNode(self, node):
-        self.nodes.add(node)
-    
+        if node.id not in self.nodes:
+            self.nodes[node.id] = node
+
     def add(self, fro, to, label=None):
         """add relation, overwrite any duplicate with same label and same ends (even if ends are swapped)"""
         l = sorted([id(fro), id(to)])
-        self.nodes.add(fro)
-        self.nodes.add(to)
-        self.relations[("a", l[0], l[1], label)] = (fro, to, label)
-        
-    def addParent(self,fro,to):
-        self.parents[(fro,to)]=(fro,to)
-    
+        self.addNode(Node(id=id(fro), label=fro))
+        self.addNode(Node(id=id(to), label=to))
+        idx = ("a", l[0], l[1], label)
+        self.relations[idx] = Relation(idx, fro, to, label=label)
+
+    def addParent(self, fro, to):
+        self.parents[(id(fro), id(to))] = (id(fro), id(to))
+        self.addNode(Node(id=id(to), label=to))
+
+
     def addKeyed(self, key, fro, to, label=None):
         """add relation, overwrite any duplicate with same key
             best used for items with a natural ordering, such as parent child, where the key is (child,parent)"""
-        self.relations[key] = (fro, to, label)
-        self.nodes.add(fro)
-        self.nodes.add(to)
-    
-    def addDualKeyed(self, key, key2, fro, to, name=None, label=None):
+        self.relations[key] = Relation(id, fro, to, label=label)
+        self.addNode(Node(id=id(fro), label=fro))
+        self.addNode(Node(id=id(to), label=to))
+
+    def addDualKeyed(self, key, key2, fro, to, label=None):
         """add relation, overwrite any duplicate with same key ends (even if they are swapped)
             best used for pairs of relations"""
         l = sorted([id(key), id(key2)])
-        
-        i=1
-        if(l[0]==id(key)):
-            i=0     
-        
-        old=None
-        if( ("dx", l[0], l[1]) in self.relations):
-            old = self.relations[("dx", l[0], l[1])][ i ];
-        
-        if(i==0):
-            self.relations[("dx", l[0], l[1])] = (fro, to, label, old, name)
-        else:
-            self.relations[("dx", l[0], l[1])] = (fro, to, label, name, old)
-               
-        self.nodes.add(fro)
-        self.nodes.add(to)
-    
+
+        idx = ("dx", l[0], l[1])
+
+        self.relations[idx] = Relation(id, fro, to, label=label)
+
+        self.addNode(Node(id=id(fro), label=fro))
+        self.addNode(Node(id=id(to), label=to))
+
     def dumpDot(self):
         dot = ""
-        
-        for node in self.nodes:
-            dot += '  "%s" [label="%s",shape=rect];\n' % (id(node), node.get_full_name())
-        
+
+        for node in self.nodes.values():
+            dot += node.to_dot()
+
         for rel in self.relations.values():
-            if rel[2] == None:
-                dot += '"%s" -- "%s";\n' % (id(rel[0]), id(rel[1]))
-            else:
-                dot += '"%s" -- "%s" [%s];\n' %(id(rel[0]), id(rel[1]), rel[2])
-        
+            dot += rel.to_dot()
+
         for rel in self.parents:
-            dot +=  '"%s" -- "%s" [dir=forward];\n' %(id(rel[0]), id(rel[1]))
-          
+            dot += '"%s" -- "%s" [dir=forward];\n' % (rel[0], rel[1])
+
         return dot
-    
+
     def dumpPlantUML(self):
         dot = ""
-        
+
         for node in self.nodes:
-            dot += 'class %s{\n' % (node.get_full_name().replace(":","_"))
+            dot += 'class %s{\n' % (node.get_full_name().replace(":", "_"))
             for attrib in node.attributes.values():
                 if not isinstance(attrib, RelationAttribute):
-                    dot+=' %s %s \n' % (getAttribType(attrib),getAttributeName(attrib)) 
+                    dot += ' %s %s \n' % (getAttribType(attrib), getAttributeName(attrib))
             dot += '}\n'
-        
+
         for rel in self.parents:
-            dot +=  '%s --> %s\n' %(rel[0].get_full_name().replace(":","_"), rel[1].get_full_name().replace(":","_"))
-            
+            dot += '%s --> %s\n' % (rel[0].get_full_name().replace(":", "_"), rel[1].get_full_name().replace(":", "_"))
+
         for rel in self.relations.values():
             if len(rel) == 5:
-                dot += '%s "%s" -- "%s" %s \n' % (rel[0].get_full_name().replace(":","_"),rel[3],rel[4], rel[1].get_full_name().replace(":","_"))
+                dot += '%s "%s" -- "%s" %s \n' % (rel[0].get_full_name().replace(":", "_"),
+                                                  rel[3], rel[4], rel[1].get_full_name().replace(":", "_"))
             else:
-                dot += '%s -- %s \n' % (rel[0].get_full_name().replace(":","_"), rel[1].get_full_name().replace(":","_"))
-          
+                dot += '%s -- %s \n' % (rel[0].get_full_name().replace(":", "_"), rel[1].get_full_name().replace(":", "_"))
+
         return dot
-        
-        
-        
+
 
 def parse_cfg(cfg):
     entries = cfg.replace("]", "").split(",")
-    
+
     result = {}
     for entry in entries:
         parts = entry.split("=")
-        
-        result[parts[0]] = parts[1]
-        
-    return result
-    
-def is_type(instance, typestring):
-    parts = typestring.split("::")
-    ns = parts[:-1]
-    t = parts[-1]
-    
-    if instance.__class__.__name__ != t:
-        return False
-        
-    if len(ns) > 0:
-        raise Exception("Not implemented")
-        
-    return True
 
+        result[parts[0]] = parts[1]
+
+    return result
+
+
+def is_type(instance, type):
+    return instance.type == type or instance.type.is_subclass(type)
 
 # FIXME: does not use relation collector, as such only dot output can use it,...
-def parse_instance(line, scope):
+def parse_instance(line, scope, collector):
     cfg = line.split("[")
     type_name = cfg[0]
     if type_name not in scope:
         return ""
 
     instances = scope[type_name].get_all_instances()
-        
+
     if len(cfg) == 2:
         cfg = parse_cfg(cfg[1])
     else:
-        cfg = {"label" : "name"}
-
-    rank = None
-    if "rank" in cfg:
-        rank = cfg["rank"]
-        del cfg["rank"]
+        cfg = {"label": "name"}
 
     dot = ""
     for instance in instances:
         options = cfg.copy()
-        if "label" in cfg and hasattr(instance, cfg["label"]):
-            options['label'] = getattr(instance, cfg["label"])
+        if "label" in cfg and cfg["label"] in instance.slots:
+            options['label'] = instance.get_attribute(cfg["label"]).value
         else:
             options['label'] = repr(instance)
 
-        options = ",".join(['%s="%s"' % x for x in options.items()])
-            
-        dot += '"%s" [%s];\n' % (instance, options)
+        #options = ",".join(['%s="%s"' % x for x in options.items()])
 
-    if rank is not None:
-        dot += "{ rank=%s; %s};\n" % (rank, "; ".join(['"%s"' % x for x in instances]))
-        
+        collector.addNode(Node(id(instance), **options))
+        #dot += '"%s" [%s];\n' % (instance, options)
+
     return dot
+
 
 def parse_class(line, scope, relcollector):
     parts = line.split("::")
-        
-    if parts[-1].startswith('*'):
-        subscope = scope.get_scope(parts[:-1])
-        typedefs = subscope.get_variables()
+    rel = False
+    parents = False
+
+    if parts[-1] == '*':
+        types = [t for (k, t) in scope.items() if re.search(line, k)]
+    elif parts[-1] == '**':
+        parts[-1] = '*'
+        rel = True
+        parents = True
+        line = "::".join(parts)
+        types = [t for (k, t) in scope.items() if re.search(line, k)]
+    elif parts[-1] == '*+':
+        parts[-1] = '*'
+        parents = True
+        line = "::".join(parts)
+        types = [t for (k, t) in scope.items() if re.search(line, k)]
     else:
-        typedefs = [scope.get_variable(parts[-1], parts[:-1])]
-    
+        types = scope[line]
+
     dot = ""
-    for type_def in typedefs:
-        type_def = type_def.value
-        if hasattr(type_def, "get_full_name"):
-            relcollector.addNode(type_def)    
-            if parts[-1] == '**':
-                addRelations(type_def, relcollector)
-        
-    
+    for type_def in types:
+
+        relcollector.addNode(Node(id(type_def), label=type_def.get_full_name()))
+        if rel:
+            addRelations(type_def, relcollector)
+        if parents:
+            addParents(type_def, relcollector)
+
     return dot
+
 
 def addRelations(entity, relcollector):
     for att in entity.get_attributes().values():
         if isinstance(att, RelationAttribute):
-            relcollector.addDualKeyed(att.end, att.end.end, entity, att.end.entity,att.get_name(),"label=" + att.get_name())
+            relcollector.addDualKeyed(att.end, att.end.end, entity, att.end.entity, att.get_name())
+            
+def addParents(entity, relcollector):
     for parent in entity.parent_entities:
         if parent.get_full_name() != "std::Entity":
             relcollector.addParent(entity, parent)
-            
-    
 
-def parse_instance_relation(link, scope, relcollector):
+
+def parse_instance_relation(link, types, relcollector):
     parts = link.split(".")
     t = parts[0]
     links = parts[1:]
-        
+
     # get the objects
-    instances = [DynamicProxy.return_value(i) for i in scope[t].get_all_instances()]
-        
+    instances = types[t].get_all_instances()
+
     for instance in instances:
-        targets = [instance]
+        targets = [("", instance)]
         for link in links:
             tfilter = None
             if "|" in link:
                 p = link.split("|")
                 if len(p) != 2:
                     raise Exception("Invalid use of | in %s" % link)
-                    
+
                 link = p[0]
                 tfilter = p[1]
-                
+                tfilter = types[tfilter]
+
             new = []
-            for x in targets:
-                result = getattr(x, link)
+            for name, x in targets:
+                result = x.get_attribute(link).value
                 if isinstance(result, list):
                     new.extend(result)
                 else:
                     new.append(result)
-                
+
             if tfilter is not None:
-                targets = [x for x in new if is_type(x, tfilter)]
+                targets.extend([(link, x) for x in new if is_type(x, tfilter)])
             else:
-                targets = new
-            
-        for target in targets:
+                targets.extend([(link, x) for x in new])
+        for link, target in targets:
             if instance is not target:
-                relcollector.add(instance, target)
+                relcollector.add(instance, target, label=link)
+
 
 def parse_class_relation(link, scope, relcollector):
     parts = link.split(".")
     t = parts[0]
     links = parts[1:]
-    
+
     if len(links) != 1:
         raise Exception("In class diagrams only one step relations are supported")
     links = links[0]
-        
+
     # get the objects
     parts = t.split("::")
     type_def = scope.get_variable(parts[-1], parts[:-1]).value
@@ -270,33 +294,35 @@ def parse_class_relation(link, scope, relcollector):
     if links == "_parents":
         for parent in type_def.parent_entities:
             relcollector.addParent(type_def, parent)
-        
+
     elif type_def.has_attribute(links):
         rel = type_def.get_attribute(links)
         relcollector.add(type_def, rel.type)
 
-def generate_dot(config, scope):
+
+def generate_dot(config, types):
     dot = "graph {\n"
-    relations = graphCollector()    
-   
-    dot += collectGraph(config, scope, relations)
-        
+    relations = graphCollector()
+
+    collectGraph(config, types, relations)
+
     dot += relations.dumpDot()
-            
+
     return dot + "}\n"
+
 
 def generate_plantUML(config, scope):
     dot = "@startuml\n"
-    relations = graphCollector()    
-   
+    relations = graphCollector()
+
     collectGraph(config, scope, relations)
-        
+
     dot += relations.dumpPlantUML()
-            
+
     return dot + "@enduml\n"
 
+
 def collectGraph(config, scope, relations):
-    dot = ""
     types = []
     links = []
     for line in config.split("\n"):
@@ -305,87 +331,83 @@ def collectGraph(config, scope, relations):
             continue
         elif "." in line:
             links.append(line)
-            
+
         elif line != "" and line[0] != " ":
             types.append(line)
 
     for t in types:
         if t[0] == "@":
-            dot += parse_class(t[1:], scope, relations)
+            parse_class(t[1:], scope, relations)
         else:
-            dot += parse_instance(t, scope)
+            parse_instance(t, scope, relations)
 
     for link in links:
         if link[0] == "@":
             parse_class_relation(link[1:], scope, relations)
         else:
             parse_instance_relation(link, scope, relations)
-    return dot
-           
+
 
 @export("graph", "graph::Graph")
 def export_graph(exporter, types):
-#    outdir = exporter.config.get("graph", "output-dir")
-    outdir="."
+    #    outdir = exporter.config.get("graph", "output-dir")
+    outdir = "."
     if outdir is None:
         return
 
     if not os.path.exists(outdir):
         os.mkdir(outdir)
-        
+
 #    if exporter.config.has_option("graph", "types"):
 #        file_types = [x.strip() for x in exporter.config.get("graph", "types").split(",")]
 #    else:
 #        file_types = []
-    file_types=["png"]
-        
+    file_types = ["png"]
+
     # Get all diagrams
     diagram_type = types["graph::Graph"]
-    
+
     for graph in diagram_type:
         dot = generate_dot(graph.config, exporter.types)
         filename = os.path.join(outdir, "%s.dot" % graph.name)
-        
+
         with open(filename, "w+") as fd:
             fd.write(dot)
-        
+
         for t in file_types:
             retcode = subprocess.call(["dot", "-T%s" % t, "-Goverlap=scale",
-                "-Gdefaultdist=0.1", "-Gsplines=true", "-Gsep=.1",
-                "-Gepsilon=.0000001", "-o", os.path.join(outdir, "%s.%s" % (graph.name, t)), filename])
-                
+                                       "-Gdefaultdist=0.1", "-Gsplines=true", "-Gsep=.1",
+                                       "-Gepsilon=.0000001", "-o", os.path.join(outdir, "%s.%s" % (graph.name, t)), filename])
+
 
 @export("classdiagram", "graph::Graph")
 def export_plantuml(exporter, types):
     #outdir = exporter.config.get("graph", "output-dir")
-    outdir="."
-    
+    outdir = "."
+
     if outdir is None:
         return
 
     if not os.path.exists(outdir):
         os.mkdir(outdir)
-        
+
 #    if exporter.config.has_option("graph", "types"):
 #        file_types = [x.strip() for x in exporter.config.get("graph", "types").split(",")]
 #    else:
 #        file_types = []
-    file_types=["png"]
+    file_types = ["png"]
 
-        
     # Get all diagrams
     diagram_type = types["graph::Graph"]
-    
+
     for graph in diagram_type:
         dot = generate_plantUML(graph.config, exporter._scope)
         filename = os.path.join(outdir, "%s.puml" % graph.name)
-        
+
         with open(filename, "w+") as fd:
             fd.write(dot)
-        
+
 #        for t in file_types:
 #            retcode = subprocess.call(["dot", "-T%s" % t, "-Goverlap=scale",
 #                "-Gdefaultdist=0.1", "-Gsplines=true", "-Gsep=.1",
 #                "-Gepsilon=.0000001", "-o", os.path.join(outdir, "%s.%s" % (graph.name, t)), filename])
-                
-                
